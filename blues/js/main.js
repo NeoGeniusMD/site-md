02eee0efeae590694d350d286f7d7b1cf3745934
NeoGeniusMD <neogenius24@gmail.com>
2019-11-01 17:34:36 +0300

    am adugat php

--- blues/js/main.js
+++ blues/js/main.js
@@ -1,10 +1,10 @@
 /* ========================================================================= */
-/*	Preloader
+/*    Preloader
 /* ========================================================================= */
 
 jQuery(window).load(function(){
 
-	$("#preloader").fadeOut("slow");
+    $("#preloader").fadeOut("slow");
 
 });
 
@@ -30,7 +30,7 @@ $(function() {
             init = function() {
 
                 initEvents();
-                
+
             },
             initEvents = function() {
 
@@ -43,30 +43,30 @@ $(function() {
                 } );
 
                 $navArrows.children( ':first' ).on( 'click', function() {
-                    
+
                     slitslider.previous();
                     return false;
 
                 } );
 
                 $nav.each( function( i ) {
-                
+
                     $( this ).on( 'click', function( event ) {
-                        
+
                         var $dot = $( this );
-                        
+
                         if( !slitslider.isActive() ) {
 
                             $nav.removeClass( 'nav-dot-current' );
                             $dot.addClass( 'nav-dot-current' );
-                        
+
                         }
-                        
+
                         slitslider.jump( i + 1 );
                         return false;
-                    
+
                     } );
-                    
+
                 } );
 
             };
@@ -83,25 +83,25 @@ $(function() {
 
 $(document).ready(function(){
 
-	/* ========================================================================= */
-	/*	Menu item highlighting
-	/* ========================================================================= */
+    /* ========================================================================= */
+    /*    Menu item highlighting
+    /* ========================================================================= */
 
-	jQuery('#nav').singlePageNav({
-		offset: jQuery('#nav').outerHeight(),
-		filter: ':not(.external)',
-		speed: 2000,
-		currentClass: 'current',
-		easing: 'easeInOutExpo',
-		updateHash: true,
-		beforeStart: function() {
-			console.log('begin scrolling');
-		},
-		onComplete: function() {
-			console.log('done scrolling');
-		}
-	});
-	
+    jQuery('#nav').singlePageNav({
+        offset: jQuery('#nav').outerHeight(),
+        filter: ':not(.external)',
+        speed: 2000,
+        currentClass: 'current',
+        easing: 'easeInOutExpo',
+        updateHash: true,
+        beforeStart: function() {
+            console.log('begin scrolling');
+        },
+        onComplete: function() {
+            console.log('done scrolling');
+        }
+    });
+
     $(window).scroll(function () {
         if ($(window).scrollTop() > 400) {
             $(".navbar-brand a").css("color","#fff");
@@ -111,64 +111,64 @@ $(document).ready(function(){
             $("#navigation").addClass("animated-header");
         }
     });
-	
-	/* ========================================================================= */
-	/*	Fix Slider Height
-	/* ========================================================================= */	
+
+    /* ========================================================================= */
+    /*    Fix Slider Height
+    /* ========================================================================= */
 
     // Slider Height
     var slideHeight = $(window).height();
-    
+
     $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
 
     $(window).resize(function(){'use strict',
         $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
     });
-	
-	
-	
-	$("#works, #testimonial").owlCarousel({	 
-		navigation : true,
-		pagination : false,
-		slideSpeed : 700,
-		paginationSpeed : 400,
-		singleItem:true,
-		navigationText: ["<i class='fa fa-angle-left fa-lg'></i>","<i class='fa fa-angle-right fa-lg'></i>"]
-	});
-	
-	
-	/* ========================================================================= */
-	/*	Featured Project Lightbox
-	/* ========================================================================= */
 
-	$(".fancybox").fancybox({
-		padding: 0,
 
-		openEffect : 'elastic',
-		openSpeed  : 650,
 
-		closeEffect : 'elastic',
-		closeSpeed  : 550,
+    $("#works, #testimonial").owlCarousel({
+        navigation : true,
+        pagination : false,
+        slideSpeed : 700,
+        paginationSpeed : 400,
+        singleItem:true,
+        navigationText: ["<i class='fa fa-angle-left fa-lg'></i>","<i class='fa fa-angle-right fa-lg'></i>"]
+    });
 
-		closeClick : true,
-			
-		beforeShow: function () {
-			this.title = $(this.element).attr('title');
-			this.title = '<h3>' + this.title + '</h3>' + '<p>' + $(this.element).parents('.portfolio-item').find('img').attr('alt') + '</p>';
-		},
-		
-		helpers : {
-			title : { 
-				type: 'inside' 
-			},
-			overlay : {
-				css : {
-					'background' : 'rgba(0,0,0,0.8)'
-				}
-			}
-		}
-	});
-	
+
+    /* ========================================================================= */
+    /*    Featured Project Lightbox
+    /* ========================================================================= */
+
+    $(".fancybox").fancybox({
+        padding: 0,
+
+        openEffect : 'elastic',
+        openSpeed  : 650,
+
+        closeEffect : 'elastic',
+        closeSpeed  : 550,
+
+        closeClick : true,
+
+        beforeShow: function () {
+            this.title = $(this.element).attr('title');
+            this.title = '<h3>' + this.title + '</h3>' + '<p>' + $(this.element).parents('.portfolio-item').find('img').attr('alt') + '</p>';
+        },
+
+        helpers : {
+            title : {
+                type: 'inside'
+            },
+            overlay : {
+                css : {
+                    'background' : 'rgba(0,0,0,0.8)'
+                }
+            }
+        }
+    });
+
 });
 
 
@@ -181,19 +181,19 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
 
-	    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
+        var myLatLng = new google.maps.LatLng(47.009186, 28.887356);
 
-	    var mapOptions = {
-	        zoom: 15,
-	        center: myLatLng,
-	        disableDefaultUI: true,
-	        scrollwheel: false,
-	        navigationControl: true,
-	        mapTypeControl: false,
-	        scaleControl: false,
-	        draggable: true,
+        var mapOptions = {
+            zoom: 15,
+            center: myLatLng,
+            disableDefaultUI: true,
+            scrollwheel: false,
+            navigationControl: true,
+            mapTypeControl: false,
+            scaleControl: false,
+            draggable: true,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             featureType: 'water',
@@ -244,7 +244,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map-canvas');
 
@@ -253,17 +253,17 @@ function init() {
 
     // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(22.402789, 91.822156),
+        position: new google.maps.LatLng(47.009186, 28.887356),
         map: map,
-		icon: 'img/icons/map-marker.png',
+        icon: 'img/icons/map-marker.png',
     });
 }
 
 // ========== END GOOGLE MAP ========== //
 
 var wow = new WOW ({
-	offset:       75,          // distance to the element when triggering the animation (default is 0)
-	mobile:       false,       // trigger animations on mobile devices (default is true)
+    offset:       75,          // distance to the element when triggering the animation (default is 0)
+    mobile:       false,       // trigger animations on mobile devices (default is true)
 });
 wow.init();
 
